72b1ec4c86099b170068ca2035a13520d6e266e5
jose-benedito <benethowen@yahoo.com.br>
2022-11-19 01:30:16 -0300

    Readaptado  api para estabelecimentos.

--- website/__init__.py
+++ website/__init__.py
@@ -28,7 +28,7 @@ def create_app():
     from .home import ind
     from .api.usuarios import api
     #from  .api.produtos import prod
-    #from  .api.negocios import estab
+    from  .api.negocios import estab
     from  .api.servicos import serv
     
 
@@ -39,7 +39,7 @@ def create_app():
     app.register_blueprint(ind, url_prefix='/')
     app.register_blueprint(api, url_prefix='/' )
     #app.register_blueprint(prod, url_prefix='/' )
-    #app.register_blueprint(estab, url_prefix='/' )
+    app.register_blueprint(estab, url_prefix='/' )
     app.register_blueprint(serv, url_prefix='/' )
     
     
