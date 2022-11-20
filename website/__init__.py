791a71fff975bae21b55bfbed25bca35d5b7be67
carla0412 <carlamazoni@yahoo.com.br>
2022-11-20 18:28:05 -0200

    alteração site

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ def create_app():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'borealis-pg-flat-99864 dpxzru6mxicrea6y2uzchvtiuom'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'borealis:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
